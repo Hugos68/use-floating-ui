418604be02ffa43d4bc6cc87557231ecb0a565f2
hugos68 <hugokorteapple@gmail.com>
2024-04-17 12:55:39 +0200

    first commit

---
+++ src/lib/useFloating.svelte.ts
@@ -0,0 +1,85 @@
+import { computePosition, autoUpdate, type ComputePositionConfig } from '@floating-ui/dom';
+
+export type FloatingContext = {
+	floatingEl: HTMLElement | null;
+	referenceEl: HTMLElement | null;
+	show: boolean;
+};
+
+export function useFloating(options: ComputePositionConfig = {}) {
+	/* State */
+	let floatingEl = $state<HTMLElement | null>(null);
+	let referenceEl = $state<HTMLElement | null>(null);
+	let show = $state(false);
+
+	/* Function */
+	async function updatePosition() {
+		if (referenceEl === null || floatingEl === null) {
+			return;
+		}
+		const position = await computePosition(referenceEl, floatingEl, options);
+		floatingEl.style.x = `${position.x}px`;
+		floatingEl.style.y = `${position.y}px`;
+	}
+
+	/* Lifecycle */
+	$effect(() => {
+		if (referenceEl === null || floatingEl === null) {
+			return;
+		}
+		return autoUpdate(referenceEl, floatingEl, updatePosition);
+	});
+
+	/* Actions */
+	function reference(node: HTMLElement, behavior: 'hover' | 'click' | 'none' = 'none') {
+		referenceEl = node;
+
+		function handleMouseEnter() {
+			if (behavior === 'hover') {
+				console.log('hover');
+				show = true;
+			}
+		}
+		function handleMouseLeave() {
+			if (behavior === 'hover') {
+				show = false;
+			}
+		}
+		function handleClick() {
+			if (behavior === 'click') {
+				show = !show;
+			}
+		}
+
+		node.addEventListener('mouseenter', handleMouseEnter);
+		node.addEventListener('mouseleave', handleMouseLeave);
+		node.addEventListener('click', handleClick);
+
+		function update(v: typeof behavior) {
+			show = false;
+			behavior = v;
+		}
+
+		function destroy() {
+			node.removeEventListener('mouseenter', handleMouseEnter);
+			node.removeEventListener('mouseleave', handleMouseLeave);
+			node.removeEventListener('click', handleClick);
+		}
+
+		return {
+			update,
+			destroy
+		};
+	}
+
+	function floating(node: HTMLElement) {
+		floatingEl = node;
+	}
+	return {
+		reference,
+		floating,
+		get show() {
+			return show;
+		}
+	};
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { useFloating } from "$lib/useFloating.svelte";
+    
+    const context = useFloating();
+</script>
+
+<section class="container">
+    <button use:context.reference={'hover'}>Reference</button>
+
+
+    <div class="floating" use:context.floating>
+    <p>Tooltip!</p>
+    </div>
+</section>
+
+<style> 
+    .floating {
+        /* Float on top of the UI */
+        position: absolute;
+        /* Avoid layout interference */
+        width: max-content;
+        top: 0;
+        left: 0;
+    }
+</style>
